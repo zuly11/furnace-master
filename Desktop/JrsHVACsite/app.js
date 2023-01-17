69e6d302d0819a092cbbfa4f37776cd1031d7528
zuly11 <zuly.rod.11@gmail.com>
2023-01-16 19:20:28 -0600

    his new site

---
+++ Desktop/JrsHVACsite/app.js
@@ -0,0 +1,28 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
